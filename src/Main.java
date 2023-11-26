ab52c0561457784bb0f70b04874baa992ce151de
Dusan Surin <surin.dusan@gmail.com>
2023-11-26 16:49:31 +0100

    upraveni kodu do finalni podobz a pripraveno k odevzdani

--- src/Main.java
+++ src/Main.java
@@ -11,60 +11,83 @@ public class Main {
        PlantList listOfPlants = new PlantList();
        try {
             listOfPlants = PlantList.loadFromFile(Settings.defaultFileName());
+           System.out.println("         ******* Nacteni seznamu kvetin ze souboru *******");
             System.out.println(listOfPlants.getPlants());
         } catch (PlantException e) {
             System.err.println("Chyba pri cteni ze souboru: "+e.getLocalizedMessage());
         }
-       //pridani 2 novych kvetin do seznamu
+
+        System.out.println("            ******* Pridani 2 kvetin do seznamu *******");
         try {
             Plant plant = new Plant(
                     "Karafiat",
                     "nemam ho rad",
                     LocalDate.of(2002, 5, 12),
                     LocalDate.of(2002, 6, 15),
-                    0
-            );
+                    0);
             listOfPlants.addPlant(plant);
         } catch (PlantException e) {
             System.err.println(e.getLocalizedMessage());
         }
         try {
-            Plant ruze = new Plant("Ruze", "je cervena", LocalDate.now(), LocalDate.now(), 1);
+            Plant ruze = new Plant(
+                    "Ruze",
+                    "je cervena",
+                    LocalDate.now(),
+                    LocalDate.now(),
+                    1);
             listOfPlants.addPlant(ruze);
         } catch (PlantException e) {
             System.err.println(e.getLocalizedMessage());;
         }
+        try {
+            Plant levandule = new Plant(
+                    "Levandule",
+                    "nekvete",
+                    LocalDate.of(2022, 5,13),
+                    LocalDate.of(2022,5,23),
+                    5);
+            listOfPlants.addPlant(levandule);
+        } catch (PlantException e) {
+            System.err.println(e.getLocalizedMessage());;
+        }
+        System.out.println();
+        System.out.println(listOfPlants.getPlants());
+
+        System.out.println("            ******* Odebrani kvetiny ze seznamu pomoci indexu*******");
+        listOfPlants.removePlant(1);
+        System.out.println(listOfPlants.getPlants());
+
 
         //13 vypis na obrazovku o zalivce
-
-        System.out.println("******* Vypis na obrazovku informace o zalivce pro vsechny kvetiny *******");
+        System.out.println("            ******* Vypis na obrazovku informace o zalivce pro vsechny kvetiny *******");
 
         listOfPlants.getWateringInfo();
-        System.out.println("*******");
 
 
 
-        System.out.println(listOfPlants);
 
-        // odebrani kvetiny s indexem 1
-        //listOfPlants.removePlant(1);
-        System.out.println(listOfPlants);
+
+
         // ulozeni seznamu do souboru
         try {
             PlantList.saveToFile("kvetinky.txt", listOfPlants);
         } catch (PlantException e) {
             System.err.println("Chyba pri zapisu do souboru: "+e.getLocalizedMessage());;
         }
-        System.out.println("serazeni rostlin podle nazvu");
+        System.out.println("\n");
+        System.out.println("            *******Serazeni rostlin podle nazvu*******");
         var plants = listOfPlants.getPlants();
         Collections.sort(plants, (first, second) -> {
             return first.getName().compareTo(second.getName());
         });
         System.out.println(plants);
-
+        System.out.println("\n");
+        System.out.println("            *******Serazeni podle posledni zalivky*******");
         Collections.sort(plants, (first, second) -> {
             return first.getWatering().compareTo(second.getWatering());
         });
+
         System.out.println(plants);
 
     }
